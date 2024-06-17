3145e62463dba6c208eff2db35390ecda76da2ef
Mohd Saquib Mansoori <msmsaq114@gmail.com>
2024-06-17 18:47:23 +0530

    Connect front and backend

--- Frontend/src/components/CountryCodeSelector.tsx
+++ Frontend/src/components/CountryCodeSelector.tsx
@@ -1,12 +1,13 @@
 import  { ChangeEvent, useState } from 'react';
 import { countryCodes } from '../utilities/countryCodes';
 
-export default function CountryCodeSelector({ handleCodeSelect }:{handleCodeSelect:(code:string)=>void})  {
+export default function CountryCodeSelector({ handleCodeSelect }:{handleCodeSelect:(code:string,countryCode:string)=>void})  {
     const [selectedCode, setSelectedCode] = useState(countryCodes[0].code);
 
     const handleSelect = (event:ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const [code,countryCode] = event.target.value.split(" ")
         setSelectedCode(event.target.value);
-        handleCodeSelect(event.target.value);
+        handleCodeSelect(code,countryCode);
     };
 
     return (
@@ -14,7 +15,7 @@ export default function CountryCodeSelector({ handleCodeSelect }:{handleCodeSele
         <label className='text-md font-medium' >Country Code</label>
         <select className= 'rounded-sm p-1 border-2 focus:outline-none border-slate-700  shadow-[5px_5px_0px_0px_rgba(0,0,0,0.3)]' title='Country Code' value={selectedCode} onChange={handleSelect}>
             {countryCodes.map((country) => (
-                <option  key={country.code} value={country.code}>
+                <option  key={country.code} value={`${country.code} ${country.countryCode}`}>
                     {country.name}   ({country.code})
                 </option>
             ))}
--- Frontend/src/components/SelectComponent.tsx
+++ Frontend/src/components/SelectComponent.tsx
@@ -1,23 +1,31 @@
 import { useContext } from "react";
 import Button from "./Button";
 import { ContextProvider } from "./Provider";
+import axios from "axios";
 
 export default function SelectComponent() {
     const context = useContext(ContextProvider)
-  
-    const handleClick = (type:string) =>{
-      context?.setType(type);
-      context?.setVisibility((currentVs:boolean) => !currentVs )
+    
+    async function handleRefresh(){
+      try{
+        await axios.get("http://localhost:4062/api/v1/refresh")
+        alert("Sheet synchronized with database succesfully")
+
+      }catch(e){
+        alert("Error in server")
+      }
     }
+    
  
 
   return  <div className={`${context?.visibility?"hidden":"block"} flex flex-col p-10  items-center h-1/2 justify-between gap-16 sm:gap-00 `}>
+            <button onClick={handleRefresh} className=" text-lg font-medium max-w-max border-2 border-slate-700 bg-slate-300  px-8 py-2 rounded-sm shadow-[5px_5px_0px_0px_rgba(0,0,0,0.3)]  hover:bg-slate-700 hover:text-slate-300">Refresh</button>
             <h1 className="md:text-5xl font-bold text-3xl ">
               SELECT A FORM
             </h1>
             <div className="flex gap-10 flex-col sm:flex-row">
-              <Button handleClickProp = {handleClick} title="FORM" type="A"  />
-              <Button handleClickProp={handleClick} title="FORM" type="B"  />
+              <Button  title="FORM" type="A"  />
+              <Button  title="FORM" type="B"  />
             </div>
         </div>
 
